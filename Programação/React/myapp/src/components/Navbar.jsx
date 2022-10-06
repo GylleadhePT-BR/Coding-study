d89235ec4f71ead7fa678fa1967ca366dc499cf3
GylleadhePT-BR <62726058+GylleadhePT-BR@users.noreply.github.com>
2022-10-05 22:07:09 -0300

    react-erro

---
+++ Programação/React/myapp/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../styles/Nav'
+import item1 from  '../styles/nav-items'
+
+function Navbar() {
+    return(
+        <Nav>
+            
+        </Nav>
+    )
+}
+
+export default Navbar
---
+++ Programação/React/myapp/src/styles/nav-items.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const item1 = styled.div`
+    width:40px;
+    height:6vh;
+    background-color: white;
+
+`
