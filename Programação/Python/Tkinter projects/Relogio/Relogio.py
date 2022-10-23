e28852f0513caad096307e8e17e8dca0a33f9c5a
GylleadhePT-BR <62726058+GylleadhePT-BR@users.noreply.github.com>
2022-10-23 15:11:49 -0300

    .

--- Programação/Python/Tkinter projects/Relogio/Relogio.py
+++ Programação/Python/Tkinter projects/Relogio/Relogio.py
@@ -1,10 +1,7 @@
-######  importanto As bibliotecas necessarias #######
 
-from tkinter import * ##### Importanto tudo do Tkinter ########
-from datetime import datetime ###### Importando biblioteca de datas e tempo ######### 
-import pyglet #### Importando biblioteca para improtar fontes personalizadas ######
-
-###### Criando Variáveis #######
+from tkinter import * 
+from datetime import datetime
+import pyglet 
 
 pyglet.font.add_file("digital-7.ttf")
 cor1 = "#3d3d3d"  # preta
@@ -16,26 +13,26 @@ cor6 = "#3080f0"  # azul
 fundo = cor1
 cor = cor2
 
-###### Iniciando tela grafica com Tkinter ########
-janela = Tk() # chamando toda a biblioteca e suas funções para uma variavel #
-janela.resizable(False , False) # informando que a janela irá er um tamanho fixo #
-janela.title("")# definindo um titulo vazio para a janela #
-janela.geometry("400x160")# Definindo resolução do programa#
-janela.configure(bg=fundo)# configurando A cor de fundo com a funcao "bg"#
 
-##### Criando função que ira armazenar todos os valores necessarios ####
+janela = Tk() 
+janela.resizable(False , False) 
+janela.title("")
+janela.geometry("400x160")
+janela.configure(bg=fundo)
+
+
 def relogio():
-    tempo = datetime.now()### defininfo a variavel "tempo" , e atribuindo o valor da função geral datetime###
-    hora = tempo.strftime("%H:%M:%S")### definindo a variavel "hora" e dessa vez atribuindo valores da hora , minutos e segundos#####
-    dia_semana = tempo.strftime("%A")### definindo a variavel "dia_semana" e dando a ela o valor apenas do dia da semana , por extenso####
-    dia_numero  =tempo.day### definindo a variavel "dia_numero" e atribuindo valor do dia da semana , porém com um valor inteiro ou seja , um numero ####
-    mes = tempo.strftime("%b")### definindo a variavel "mes" e dando valor da função de tempo , porem apenas o valor do mes ####
-    ano = tempo.strftime("%y")### definindo a variavel "ano" e dando valor igual ao ano atual###
+    tempo = datetime.now()
+    hora = tempo.strftime("%H:%M:%S")
+    dia_semana = tempo.strftime("%A")
+    dia_numero  =tempo.day
+    mes = tempo.strftime("%b")
+    ano = tempo.strftime("%y")
 
 
-    ### após Criar os campos gráficos###
-    Hora.config(text=hora)### definindo configuração da label "Hora" que criamos para alterar o seu texto e incorporar o valor da varialvel "hora"###
-    Hora.after(200 , relogio)### Para deixar o relogio dinamico e nao estácio , precisamos fazer com que ele se atualize a cada 200 milisegudos###
+    
+    Hora.config(text=hora)
+    Hora.after(200 , relogio)
     Data.config(text=dia_semana+"     "+str(dia_numero) + "/" + str(mes)+ "/" + str(ano))
     
 
