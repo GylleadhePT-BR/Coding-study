99e222a470aec6cd8f2fe7148330dabce11c0ba2
GylleadhePT-BR <62726058+GylleadhePT-BR@users.noreply.github.com>
2022-10-27 12:30:01 -0300

    u

--- Programação/Python/Tkinter projects/Relogio/Relogio.py
+++ Programação/Python/Tkinter projects/Relogio/Relogio.py
@@ -1,7 +1,7 @@
 
-from tkinter import * 
+from tkinter import *
 from datetime import datetime
-import pyglet 
+import pyglet
 
 pyglet.font.add_file("digital-7.ttf")
 cor1 = "#3d3d3d"  # preta
@@ -14,8 +14,8 @@ fundo = cor1
 cor = cor2
 
 
-janela = Tk() 
-janela.resizable(False , False) 
+janela = Tk()
+janela.resizable(False, False)
 janela.title("")
 janela.geometry("400x160")
 janela.configure(bg=fundo)
@@ -25,23 +25,21 @@ def relogio():
     tempo = datetime.now()
     hora = tempo.strftime("%H:%M:%S")
     dia_semana = tempo.strftime("%A")
-    dia_numero  =tempo.day
+    dia_numero = tempo.day
     mes = tempo.strftime("%b")
     ano = tempo.strftime("%y")
 
-
-    
     Hora.config(text=hora)
-    Hora.after(200 , relogio)
-    Data.config(text=dia_semana+"     "+str(dia_numero) + "/" + str(mes)+ "/" + str(ano))
-    
+    Hora.after(200, relogio)
+    Data.config(text=dia_semana+"     "+str(dia_numero) +
+                "/" + str(mes) + "/" + str(ano))
 
 
-Hora = Label(janela , text="" , font=("digital-7 80") , bg=fundo , fg=cor6)
-Hora.grid(row=0 , column=0 , sticky=NW , padx=60)
+Hora = Label(janela, text="", font=("digital-7 80"), bg=fundo, fg=cor6)
+Hora.grid(row=0, column=0, sticky=NW, padx=60)
 
-Data = Label(janela , text="" , font=("digital-7 20 ") , bg=fundo  , fg=cor)
-Data.grid(row=1 , column=0 ,  sticky=NW , padx=80)
+Data = Label(janela, text="", font=("digital-7 20 "), bg=fundo, fg=cor)
+Data.grid(row=1, column=0,  sticky=NW, padx=80)
 
 
 relogio()
